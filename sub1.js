3b5c2b6b4f53fe69f45f9daa79254eaac96678d4
Paul Schröder <paulschroeder05@outlook.de>
2023-09-11 13:06:34 +0200

    Update

--- sub1.js
+++ sub1.js
@@ -1,13 +1,15 @@
-function useTemplate() {
+function useTemplate(i) {
     var myTemplate = document.getElementById('template'),
         normalContent = document.getElementById('content'),
         clonedTemplate = myTemplate.content.cloneNode(true);
     normalContent.appendChild(clonedTemplate);
 }
 
+document.getElementById("title").textContent = "Moinsen";
+
 var counter = 0;
 
 while (counter < 1000) {
-    useTemplate();
+    useTemplate(counter);
     counter++;
 }
