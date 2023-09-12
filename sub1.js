9b9ce1b0cba1390777bf1c07da1158316a4dae45
Paul Schröder <paulschroeder05@outlook.de>
2023-09-12 18:50:18 +0200

    js

--- sub1.js
+++ sub1.js
@@ -1,14 +1,41 @@
-function useTemplate(i) {
-    var myTemplate = document.getElementById('template'),
-        normalContent = document.getElementById('content'),
-        clonedTemplate = myTemplate.content.cloneNode(true);
+var i = 1;
 
-    normalContent.appendChild(clonedTemplate);
-}
+while (i < 120) {
+    var content = document.getElementById("content"),
+        section = document.createElement("section"),
+        img = document.createElement("img"),
+        p = document.createElement("p"),
+        select = document.createElement("select"),
+        optionXS = document.createElement("option"),
+        optionS = document.createElement("option"),
+        optionM = document.createElement("option"),
+        optionL = document.createElement("option"),
+        optionXL = document.createElement("option"),
+        optionXXL = document.createElement("option"),
+        button = document.createElement("button");
 
-var counter = 0;
+    section.className = "product";
+    img.className = "image";
+    p.className = "product-info info";
+    select.className = "size info";
+    button.className = "to-cart info";
 
-while (counter < 1000) {
-    useTemplate(counter);
-    counter++;
+    try {
+        img.src = "Medien/Kleidung/"+i+"_h.jpg";
+    } catch (error) {
+
+    }
+    p.textContent = i;
+    optionXS.textContent = "XS";
+    optionS.textContent = "S";
+    optionM.textContent = "M";
+    optionL.textContent = "L";
+    optionXL.textContent = "XL";
+    optionXXL.textContent = "XXL";
+
+    select.append(optionXS, optionS, optionM, optionL, optionXL, optionXXL);
+    section.append(img, p, select, button);
+    content.append(section);
+
+    i++
 }
