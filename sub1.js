97eabb7fe6a1a0c997c96beca08b4eb43d5647c2
Paul Schröder <paulschroeder05@outlook.de>
2023-09-13 09:44:32 +0200

    Update everything

--- sub1.js
+++ sub1.js
@@ -1,41 +1,48 @@
 var i = 1;
+const ban = [26, 28, 29, 33, 37, 39, 42, 45, 46, 65, 66, 67, 68, 69, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 113, 115, 116, 117, 118, 119];
 
-while (i < 120) {
-    var content = document.getElementById("content"),
-        section = document.createElement("section"),
-        img = document.createElement("img"),
-        p = document.createElement("p"),
-        select = document.createElement("select"),
-        optionXS = document.createElement("option"),
-        optionS = document.createElement("option"),
-        optionM = document.createElement("option"),
-        optionL = document.createElement("option"),
-        optionXL = document.createElement("option"),
-        optionXXL = document.createElement("option"),
-        button = document.createElement("button");
+while (i < 140) {
 
-    section.className = "product";
-    img.className = "image";
-    p.className = "product-info info";
-    select.className = "size info";
-    button.className = "to-cart info";
-
-    try {
-        img.src = "Medien/Kleidung/"+i+"_h.jpg";
-    } catch (error) {
-
+    if (ban.includes(i)) {
+        i++;
     }
-    p.textContent = i;
-    optionXS.textContent = "XS";
-    optionS.textContent = "S";
-    optionM.textContent = "M";
-    optionL.textContent = "L";
-    optionXL.textContent = "XL";
-    optionXXL.textContent = "XXL";
+    else {
+        var content = document.getElementById("content"),
+            section = document.createElement("section"),
+            img = document.createElement("img"),
+            p = document.createElement("p"),
+            select = document.createElement("select"),
+            optionXS = document.createElement("option"),
+            optionS = document.createElement("option"),
+            optionM = document.createElement("option"),
+            optionL = document.createElement("option"),
+            optionXL = document.createElement("option"),
+            optionXXL = document.createElement("option"),
+            button = document.createElement("button");
 
-    select.append(optionXS, optionS, optionM, optionL, optionXL, optionXXL);
-    section.append(img, p, select, button);
-    content.append(section);
+        section.className = "product";
+        img.className = "image";
+        p.className = "product-info info";
+        select.className = "size info";
+        button.className = "to-cart info";
 
-    i++
+        try {
+            img.src = "Medien/Kleidung/" + i + "_h.jpg";
+        } catch (error) {
+
+        }
+        p.textContent = i;
+        optionXS.textContent = "XS";
+        optionS.textContent = "S";
+        optionM.textContent = "M";
+        optionL.textContent = "L";
+        optionXL.textContent = "XL";
+        optionXXL.textContent = "XXL";
+
+        select.append(optionXS, optionS, optionM, optionL, optionXL, optionXXL);
+        section.append(img, p, select, button);
+        content.append(section);
+
+        i++;
+    }
 }
