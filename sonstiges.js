57ad59f398608d4de02f6cf144dae5b789854f26
PaulusMaulusIII <paulschroeder05@outlook.de>
2023-10-02 20:34:03 +0200

    Added search funtion to all pages

--- sonstiges.js
+++ sonstiges.js
@@ -275,3 +275,28 @@ colorSelect.addEventListener("change", () => gen());
 typeSelect.addEventListener("change", () => gen());
 
 motiveSelect.addEventListener("change", () => gen());
+
+const openPopupButton = document.getElementById("suchen"),
+    closePopupButton = document.getElementById("closePopup"),
+    overlay = document.getElementById("overlay"),
+    popup = document.getElementById("popup"),
+    enter = document.getElementById("eingabe");
+
+openPopupButton.addEventListener("click", () => {
+    overlay.style.display = "block";
+    popup.style.display = "flex";
+});
+
+closePopupButton.addEventListener("click", () => {
+    overlay.style.display = "none";
+    popup.style.display = "none";
+});
+
+overlay.addEventListener("click", () => {
+    overlay.style.display = "none";
+    popup.style.display = "none";
+});
+
+enter.addEventListener("click", () => {
+    window.location.href = "http://localhost/results.html?search=" + document.getElementById("searchbar").value;
+})
