d98592ef434637c8c154ac54a35c7d21e45dbf2e
PaulusMaulusIII <paulschroeder05@outlook.de>
2023-10-05 11:24:38 +0200

    Changed cart to save locally

--- JS/wagen.js
+++ JS/wagen.js
@@ -1,20 +1,58 @@
 const CartButton = document.getElementById("cart"),
-    toCartButton = document.getElementsByClassName("toCart"),
-    cartContents = [],
-    cartPopup = document.getElementById("cartPopup");
+    toCartButtons = document.getElementsByClassName("toCart"),
+    cartPopup = document.getElementById("cartPopup"),
+    clearCartButton = document.getElementById("clearCartButton"),
+    cartDisplay = document.getElementById("cartContentP");
+
+let userCart = {
+    items: []
+};
+
+const saveCartToLocalStorage = () => {
+    localStorage.setItem('userCart', JSON.stringify(userCart));
+}
+
+const loadCartFromLocalStorage = () => {
+    let cartData = localStorage.getItem('userCart');
+    if (cartData) {
+        userCart = JSON.parse(cartData);
+        displayCart();
+    }
+}
 
 const openPopup = () => {
     cartPopup.style.display = "flex";
+    displayCart();
 };
 
 const closePopup = () => {
-    cartPopup.style.display = "flex";
+    cartPopup.style.display = "none";
 };
 
 const addToCart = (id) => {
-    cartContents.push(id);
+    userCart.items.push(id);
+    console.log(userCart.items);
+    saveCartToLocalStorage();
+};
+
+const clearCart = () => {
+    userCart.items = [];
+    localStorage.removeItem('userCart');
+};
+
+const displayCart = () => {
+    cartDisplay.textContent = userCart.items;
 };
 
 CartButton.addEventListener("mouseenter", () => openPopup());
 CartButton.addEventListener("mouseover", () => openPopup());
 CartButton.addEventListener("mouseleave", () => closePopup());
+
+if (clearCartButton) {
+    clearCartButton.addEventListener("click", () => {
+        clearCart();
+        displayCart(); // Update the cart display after clearing the cart
+    });
+}
+
+loadCartFromLocalStorage();
