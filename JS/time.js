17415f4b740ab7a8c7e0c2e819c5cd2bae829917
Paul Schröder <paulschroeder05@outlook.de>
2023-11-20 18:26:27 +0100

    Fix?

--- JS/time.js
+++ JS/time.js
@@ -1,35 +1,38 @@
 const getDate = async () => {
     return await fetch(settings.backend_ip + "orderDate").then(response => response.text());
 }
-console.log(getDate(), Date(getDate()));
-let end = new Date(getDate());
-console.log(end);
-end = end.getTime();
-console.log(end);
-const dateElement = document.getElementById("date");
 
-const _second = 1000;
-const _minute = _second * 60;
-const _hour = _minute * 60;
-const _day = _hour * 24;
-let timer;
+const createCountdown = async () => {
+    console.log(getDate(), Date(getDate()));
+    let end = Date(await getDate());
+    console.log(end);
+    end = end.getTime();
+    console.log(end);
+    const dateElement = document.getElementById("date");
 
-function showRemaining() {
-    const now = new Date();
-    const distance = end - now;
-    if (distance < 0) {
+    const _second = 1000;
+    const _minute = _second * 60;
+    const _hour = _minute * 60;
+    const _day = _hour * 24;
+    let timer;
 
-        clearInterval(timer);
-        dateElement.innerHTML = 'Abgelaufen!';
+    function showRemaining() {
+        const now = new Date();
+        const distance = end - now;
+        if (distance < 0) {
 
-        return;
+            clearInterval(timer);
+            dateElement.innerHTML = 'Abgelaufen!';
+
+            return;
+        }
+        const days = Math.floor(distance / _day);
+        const hours = Math.floor((distance % _day) / _hour);
+        const minutes = Math.floor((distance % _hour) / _minute);
+        const seconds = Math.floor((distance % _minute) / _second);
+
+        dateElement.innerHTML = `Nur noch ${days} Tage, ${hours} Std., ${minutes} Min. und ${seconds} Sek. bis zur nächsten Bestellung!`;
     }
-    const days = Math.floor(distance / _day);
-    const hours = Math.floor((distance % _day) / _hour);
-    const minutes = Math.floor((distance % _hour) / _minute);
-    const seconds = Math.floor((distance % _minute) / _second);
 
-    dateElement.innerHTML = `Nur noch ${days} Tage, ${hours} Std., ${minutes} Min. und ${seconds} Sek. bis zur nächsten Bestellung!`;
+    timer = setInterval(showRemaining, 1000);
 }
-
-timer = setInterval(showRemaining, 1000);
