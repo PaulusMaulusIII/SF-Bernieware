0ccbf757832760426f968d2840dccdc4cb0f9f13
Paul Schröder <paulschroeder05@outlook.de>
2023-11-24 18:54:18 +0100

    Added ParseCSV class

--- JS/search.js
+++ JS/search.js
@@ -3,7 +3,35 @@ const openPopupButton = document.getElementById("search"),
     overlay = document.getElementById("overlay"),
     popup = document.getElementById("searchPopup"),
     enter = document.getElementById("eingabe"),
-    searchbar = document.getElementById("searchbar");
+    searchbar = document.getElementById("searchbar"),
+    autocompleteField = document.getElementById("autocomplete");
+
+let results = new Map();
+
+const searchCSV = {
+
+    getCSV: async (search) => {
+        await fetch(settings.backend_ip + "database.csv") //Fetch zieht die tabelle als HTML
+            .then(response => response.text()) //HTML zu String
+            .then(async (response) => {
+                console.log(response);
+                for (const element of CSVParser.parse(response)) {
+                    for (const el of element) {
+                        if (el.toUpperCase().includes(search.toUpperCase()) && !results.has(el)) {
+                            results.set(el, element);
+                        }
+                    }
+                }
+            })
+            .catch(err => console.log(err));
+
+        try {
+            document.getElementById("content").removeChild(document.getElementById("loading")); // "Bitte warten ... wird entfernt, falls es noch da ist"
+        } catch (error) {
+
+        }
+    },
+}
 
 openPopupButton.addEventListener("click", () => {
     overlay.style.display = "block";
@@ -22,11 +50,15 @@ overlay.addEventListener("click", () => {
 });
 
 enter.addEventListener("click", () => {
-    window.location.href = "sub.html?search="+document.getElementById("searchbar").value;
+    window.location.href = "sub.html?search=" + document.getElementById("searchbar").value;
 });
 
 searchbar.addEventListener("keypress", (evt) => {
     if (evt.code === "Enter") {
-        window.location.href = "sub.html?search="+document.getElementById("searchbar").value;
+        window.location.href = "sub.html?search=" + document.getElementById("searchbar").value;
     }
+});
+
+searchbar.addEventListener("input", () => {
+
 });
